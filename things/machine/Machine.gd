cc39c7bdcc5f9999d7c0f78a62cc91b8b7d1456f
Rien Maertens <rien.maertens@posteo.be>
2021-07-10 22:51:58 +0200

    Walls to platforms

--- things/machine/Machine.gd
+++ things/machine/Machine.gd
@@ -9,6 +9,8 @@ onready var _camera: Camera = get_viewport().get_camera()
 var _saved_camera_zoom: Vector2
 var _zoom_out: Vector2 = Vector2(3, 3)
 
+export(Array, NodePath) var influencees: Array
+
 func _on_Area2D_body_entered(body: PhysicsBody2D):
 	if not body.is_in_group("player"):
 		return
@@ -37,3 +39,5 @@ func _on_Area2D_body_exited(body):
 	_tween.interpolate_property(camera, "zoom", camera.zoom, _saved_camera_zoom, .4, Tween.TRANS_SINE)
 	_tween.interpolate_property(camera, "position", camera.position, Vector2.ZERO, .4, Tween.TRANS_BACK)
 	_tween.start()
+	
+
