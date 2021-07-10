92cb9e6d0ad75220f8de12f180b86a41f905527e
Rien Maertens <rien.maertens@posteo.be>
2021-07-10 16:17:21 +0200

    Friction!

--- things/player/Player.gd
+++ things/player/Player.gd
@@ -5,12 +5,15 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 
-export var _speed: int = 200
+export var _friction_floor: float = .1
+export var _friction_air: float = .01
+export var _speed_stop: float = 1
+export var _speed: int = 800
 export var _max_speed: float = 250.0
 export(float) var _jump_strength: float = 300
 export(NodePath) var _center_object: NodePath
 
-export(float) var gravity: float = 300.0
+export(float) var gravity: float = 600.0
 
 
 var _center: Vector2 = Vector2.ZERO
@@ -26,24 +29,50 @@ func _ready():
 func _physics_process(delta):
 	var new_rotation: float = get_angle_to(_center) - PI / 2
 	rotate(new_rotation)
-	_up = Vector2.UP.rotated(new_rotation)
-	print(_center)
+	_up = -Vector2.UP.rotated(new_rotation)
 	
 	var movement: Vector2 = Vector2.ZERO
+	var relative_velocity =  _velocity.rotated(-rotation)
+	print(relative_velocity)
+	$Velocity.points = PoolVector2Array([Vector2(0, 0), relative_velocity])
 	
+	# Friction
+	var friction = _friction_air
+	if abs(relative_velocity.x) < _speed_stop:
+		friction = 1
+	elif is_on_floor():
+		friction = _friction_floor
+	movement.x = -relative_velocity.x * friction
+
 	# Gravity
-	movement.y += gravity * delta
+	var down_force: float = gravity * delta
+	movement.y += down_force
+	$Gravity.points = PoolVector2Array([Vector2(0, 0), Vector2(0, down_force * 10)]) 
 	
 	# X movement
-	movement += handle_sideways_movement(delta)
+	var sideways = handle_sideways_movement(delta, relative_velocity)
+	$Sideways.points = PoolVector2Array([Vector2(0, 0), sideways * 10])
+	movement += sideways
+	
 	# Jumping
-	movement += handle_jump()
+	var jump = handle_jump()
+	$Jump.points = PoolVector2Array([Vector2(0, 0), jump * 10])
+	movement += jump
 	
-	_velocity += movement.rotated(rotation)
 	
-	$Line2D.points = PoolVector2Array([Vector2(0, 0), _up * 100])
+	if is_on_floor():
+		print("FLOOR")
+	if is_on_wall():
+		print("WALL")
+	if is_on_ceiling():
+		print("CEILING")
 	
+	var _rotated_movement = movement.rotated(rotation)
+
+	_velocity += _rotated_movement
 	_velocity = move_and_slide(_velocity, _up)
+	print(_velocity)
+
 
 
 func handle_jump() -> Vector2:
@@ -54,7 +83,7 @@ func handle_jump() -> Vector2:
 	return Vector2(0, -_jump_strength)
 	
 	
-func handle_sideways_movement(delta) -> Vector2:
+func handle_sideways_movement(delta: float, relative_velocity: Vector2) -> Vector2:
 	var _direction: Vector2 = Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		0
